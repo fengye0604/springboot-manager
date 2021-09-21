9effc4adf85328dc0ee2a068712eb469e430e64e
fengye0604 <1476994341@qq.com>
2021-09-21 15:41:44 +0800

    '提交修改'

---
+++ example-common/src/main/java/com/example/examplecommon/model/MyToken.java
@@ -0,0 +1,11 @@
+package com.example.examplecommon.model;
+
+import lombok.Data;
+
+/**
+ *
+ */
+@Data
+public class MyToken {
+    private String token = "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";
+}
